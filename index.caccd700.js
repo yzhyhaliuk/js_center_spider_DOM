c5e514235d3e9ecd7c197df340e1d0fe5d1875f7
Yana <anazigaluk@gmail.com>
2024-12-06 18:54:35 +0200

    make build

---
+++ index.caccd700.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.top=e.clientHeight-e.clientHeight/2-t.offsetHeight/2+"px",t.style.left=e.clientWidth-e.clientWidth/2-t.offsetWidth/2+"px";
+//# sourceMappingURL=index.caccd700.js.map
